297421a397f851ec9490f97e6e3d244fbd6460d0
Korka13 <korka1313@gmail.com>
2019-07-12 08:36:31 +0000

    Updates

---
+++ precache-manifest.02efccbe857f2fcd47029d0b68291f54.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd8dad08c54573ce80e971e9a03b5e68",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "aae5b9f065d6138eccf8",
+    "url": "/portfolio/static/css/main.4023fb7c.chunk.css"
+  },
+  {
+    "revision": "c883f23eb5c98b6ba556",
+    "url": "/portfolio/static/js/2.eca68da6.chunk.js"
+  },
+  {
+    "revision": "aae5b9f065d6138eccf8",
+    "url": "/portfolio/static/js/main.81aadd6d.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "69365c4199898ebfed610d9d18525885",
+    "url": "/portfolio/static/media/ASDA_logo.69365c41.jpg"
+  },
+  {
+    "revision": "009d90fb6bcab5ca07e6601c8788cc88",
+    "url": "/portfolio/static/media/background.009d90fb.gif"
+  },
+  {
+    "revision": "dfa3ffc1d88edf290e90a5bdc01c67da",
+    "url": "/portfolio/static/media/cosmote.dfa3ffc1.jpg"
+  },
+  {
+    "revision": "c9a7e6c410eb884745467bf076463f17",
+    "url": "/portfolio/static/media/extraordinary-test.c9a7e6c4.gif"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/portfolio/static/media/github.76d344f0.svg"
+  },
+  {
+    "revision": "99b0f557b75f06e1cb4c43376c9595c3",
+    "url": "/portfolio/static/media/guess.99b0f557.gif"
+  },
+  {
+    "revision": "72800fae2fe4fbe81639c9f15ab120f8",
+    "url": "/portfolio/static/media/gull12.72800fae.png"
+  },
+  {
+    "revision": "7ccfdd985dbdb018dc95dce79e309e62",
+    "url": "/portfolio/static/media/instagen.7ccfdd98.png"
+  },
+  {
+    "revision": "5b238434e2862c877f08572b96c0ef7b",
+    "url": "/portfolio/static/media/linkedin.5b238434.svg"
+  },
+  {
+    "revision": "de494ce03cedd9adea5eb79bc652ef97",
+    "url": "/portfolio/static/media/teleperformance.de494ce0.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f91c289eaa39da91557582aa57039e10.js"
+  "/portfolio/precache-manifest.02efccbe857f2fcd47029d0b68291f54.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
