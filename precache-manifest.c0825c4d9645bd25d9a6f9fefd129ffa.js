1c48a59c484abee546429a35ef79426d9ae0c3c2
Korka13 <korka1313@gmail.com>
2019-07-12 08:23:56 +0000

    Updates

---
+++ precache-manifest.c0825c4d9645bd25d9a6f9fefd129ffa.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87d8408d5ae231fe6f9dd634497dac67",
+    "url": "/Korka13/portfolio/index.html"
+  },
+  {
+    "revision": "874e3684e9a779db049b",
+    "url": "/Korka13/portfolio/static/css/main.4023fb7c.chunk.css"
+  },
+  {
+    "revision": "c883f23eb5c98b6ba556",
+    "url": "/Korka13/portfolio/static/js/2.eca68da6.chunk.js"
+  },
+  {
+    "revision": "874e3684e9a779db049b",
+    "url": "/Korka13/portfolio/static/js/main.364dd2e7.chunk.js"
+  },
+  {
+    "revision": "32aa45cba407f80db045",
+    "url": "/Korka13/portfolio/static/js/runtime~main.846d5cb8.js"
+  },
+  {
+    "revision": "69365c4199898ebfed610d9d18525885",
+    "url": "/Korka13/portfolio/static/media/ASDA_logo.69365c41.jpg"
+  },
+  {
+    "revision": "009d90fb6bcab5ca07e6601c8788cc88",
+    "url": "/Korka13/portfolio/static/media/background.009d90fb.gif"
+  },
+  {
+    "revision": "dfa3ffc1d88edf290e90a5bdc01c67da",
+    "url": "/Korka13/portfolio/static/media/cosmote.dfa3ffc1.jpg"
+  },
+  {
+    "revision": "c9a7e6c410eb884745467bf076463f17",
+    "url": "/Korka13/portfolio/static/media/extraordinary-test.c9a7e6c4.gif"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/Korka13/portfolio/static/media/github.76d344f0.svg"
+  },
+  {
+    "revision": "99b0f557b75f06e1cb4c43376c9595c3",
+    "url": "/Korka13/portfolio/static/media/guess.99b0f557.gif"
+  },
+  {
+    "revision": "72800fae2fe4fbe81639c9f15ab120f8",
+    "url": "/Korka13/portfolio/static/media/gull12.72800fae.png"
+  },
+  {
+    "revision": "7ccfdd985dbdb018dc95dce79e309e62",
+    "url": "/Korka13/portfolio/static/media/instagen.7ccfdd98.png"
+  },
+  {
+    "revision": "5b238434e2862c877f08572b96c0ef7b",
+    "url": "/Korka13/portfolio/static/media/linkedin.5b238434.svg"
+  },
+  {
+    "revision": "de494ce03cedd9adea5eb79bc652ef97",
+    "url": "/Korka13/portfolio/static/media/teleperformance.de494ce0.jpg"
+  }
+]);
